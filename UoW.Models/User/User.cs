40d2cd991d7e5baf25359e6d04b57b05683f94a2
b.serafimov <b.b.serafimov@gmail.com>
2020-10-07 20:38:43 +0300

    Initial setup

---
+++ UoW.Models/User/User.cs
@@ -0,0 +1,9 @@
+﻿namespace UoW.Models.User
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int PersonalNumber { get; set; }
+        public int TeamId { get; set; }
+    }
+}
---
+++ UoW/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UoW.Models.User;
+
+namespace TroubleT.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : Controller
+    {
+        public UserController()
+        {
+
+        }
+
+        [HttpGet]
+        public User GetUser()
+        {
+            return new User()
+            {
+                PersonalNumber = 1,
+                Name = "Boris",
+                TeamId = 12
+            };
+        }
+
+    }
+}
