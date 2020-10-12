ed280374a7a3fb45d2661ef0a9ecff556ada1743
Vasil Ivanov <vasivanov.info@gmail.com>
2020-10-12 20:26:15 +0300

    update

---
+++ UoW.Models/Users/Specialty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UoW.Models.User
+{
+    public class Specialty
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string ShortName { get; set; }
+
+        public int FacultyID { get; set; }
+        public int LectorID { get; set; }
+    }
+}
---
+++ UoW/Controllers/SpecialtyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UoW.Models.User;
+
+namespace UoW.Controllers
+{
+    [Route("specialty")]
+    [ApiController]
+    public class SpecialtyController : ControllerBase
+    {
+        public SpecialtyController()
+        {
+
+        }
+
+        [HttpGet]
+        public Specialty GetSpecialty()
+        {
+            return new Specialty()
+            {
+                FacultyID = 1,
+                ID = 12,
+                LectorID = 123,
+                Name = "vasil",
+                ShortName = "vas"
+            };
+        }
+    }
+}
