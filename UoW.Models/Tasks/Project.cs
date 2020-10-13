0927c3942eb6323e69cbd1179e91bf050e5dcfac
EKIT LAB 315 <EKIT LAB 315@DESKTOP-CDSK1OD>
2020-10-13 16:40:25 +0300

    project

---
+++ UoW.Models/Tasks/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using UoW.Models.User;
+
+namespace UoW.Models.Tasks
+{
+    
+   public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ProductOwnerID { get; set; }
+
+    }
+}
---
+++ UoW/Controllers/ProjectController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UoW.Models.Tasks;
+
+namespace UoW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        [HttpGet]
+        public Project GetProject()
+        {
+            return new Project()
+            {
+                Description = "hello",
+                ID = 5,
+                Name = "Stoyan",
+                ProductOwnerID = 3,
+            
+
+            };
+        }
+    }
+}
