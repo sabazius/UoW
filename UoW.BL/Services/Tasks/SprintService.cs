54396c5f77ca744e157c381c3a9e16886436975e
b.serafimov <b.b.serafimov@gmail.com>
2020-10-20 00:20:16 +0300

    Merge TeamService

---
+++ UoW.BL/Services/Tasks/SprintService.cs
@@ -0,0 +1,20 @@
+﻿using UoW.BL.Interface.Tasks;
+using UoW.DL.Interfaces.Users;
+using UoW.Models.Tasks;
+
+namespace UoW.BL.Services.Tasks
+{
+    public class SprintService : ISprintService
+    {
+        ISprintRepository _sprintRepository;
+        public SprintService(ISprintRepository sprintRepository)
+        {
+            _sprintRepository = sprintRepository;
+        }
+
+        public Sprint GetSprintById(int id)
+        {
+            return _sprintRepository.GetById(id);
+        }
+    }
+}
