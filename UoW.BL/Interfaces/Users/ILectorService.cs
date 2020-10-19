aac09647b841a75df63342aecd64d3f941dba858
b.serafimov <b.b.serafimov@gmail.com>
2020-10-20 00:25:24 +0300

    merge lector

---
+++ UoW.BL/Interfaces/Users/ILectorService.cs
@@ -0,0 +1,9 @@
+﻿using UoW.Models.Users;
+
+namespace UoW.BL.Interfaces.Users
+{
+    public interface ILectorService
+    {
+        Lector GetLectorId(int Id);
+    }
+}
---
+++ UoW.BL/Services/Users/LectorService.cs
@@ -0,0 +1,20 @@
+﻿using UoW.BL.Interfaces.Users;
+using UoW.DL.Interfaces.Users;
+using UoW.Models.Users;
+
+namespace UoW.BL.Services.Users
+{
+    public class LectorService : ILectorService
+    {
+        ILectorRepository _lectorRepository;
+
+        public LectorService(ILectorRepository lectorRepository)
+        {
+            _lectorRepository = lectorRepository;
+        }
+        public Lector GetLectorId(int Id)
+        {
+            return _lectorRepository.GetById(Id);
+        }
+    }
+}
