2809c3e2989b6306a9174b0e3dba643b8d423a03
b.serafimov <b.b.serafimov@gmail.com>
2020-11-05 18:56:25 +0200

    UserPositionRequestValidator

---
+++ UoW/Validators/UserPositionRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using UoW.Models.Contracts.Requests;
+
+namespace UoW.Validators
+{
+	public class UserPositionRequestValidator : AbstractValidator<UserPositionRequest>
+	{
+		public UserPositionRequestValidator()
+		{
+			RuleFor(x => x.Id).GreaterThan(0);
+			RuleFor(x => x.Description).MaximumLength(200);
+			RuleFor(x => x.PositionName).MaximumLength(30);
+		}
+	}
+}
