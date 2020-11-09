4f485c4ed4e3f3de399704e26a32f4217b5c64a7
Vasil Ivanov <vasivanov.info@gmail.com>
2020-11-09 16:45:11 +0200

    validate specialty request with fluent validator

--- UoW/Validators/SpecialtyRequestValidator.cs
+++ UoW/Validators/SpecialtyRequestValidator.cs
@@ -12,6 +12,12 @@ namespace UoW.Validators
         public SpecialtyRequestValidator()
         {
             RuleFor(x => x.Id).GreaterThan(0);
+            RuleFor(x => x.Name).MaximumLength(25);
+            RuleFor(x => x.Name).MinimumLength(5);
+            RuleFor(x => x.FacultyId).GreaterThan(0);
+            RuleFor(x => x.LectorId).GreaterThan(0);
+            RuleFor(x => x.ShortName).MaximumLength(5);
+            RuleFor(x => x.ShortName).MinimumLength(2);
         }
     }
 }
