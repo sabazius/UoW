06ec6b7696d5ce54b2b89d62e1c8dcfbbbfa904b
b.serafimov <b.b.serafimov@gmail.com>
2020-11-12 17:08:30 +0200

    Lector async and mongo

--- UoW.DL/Interfaces/Users/IFacultyRepository.cs
+++ UoW.DL/Interfaces/Users/IFacultyRepository.cs
@@ -1,12 +1,13 @@
-﻿using UoW.Models.Users;
+﻿using System.Threading.Tasks;
+using UoW.Models.Users;
 
 namespace UoW.DL.Interfaces.Users
 {
     public interface IFacultyRepository
     {
-    	void Create(Faculty user);
-        void Delete(int userId);
-        void Update(Faculty user);
-        Faculty GetById(int userId);
+    	Task<Faculty> Create(Faculty user);
+        Task Delete(int userId);
+        Task<Faculty> Update(Faculty user);
+        Task<Faculty> GetById(int userId);
     }
 }
